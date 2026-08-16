b735d98f4e5883c2c43d8cd0b735e8265263266c
satesatelight-afk <satesatelight.gmail.com>
2026-08-16 23:24:34 +0900

    Add staging deployment step

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+set -e
+BUCKET="fastapi-files-20260816"
+COMMIT_ID=$1
+APP_DIR="/home/ec2-user/fastapi-app"
+BUILD_DIR="/tmp/fastapi-build"
+
+if [ -z "$COMMIT_ID" ]; then
+  echo "Error: COMMIT_ID is required"
+  exit 1
+fi
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+aws s3 cp s3://$BUCKET/builds/app-$COMMIT_ID.tar.gz .
+if [ -d "$APP_DIR" ]; then
+  sudo mv $APP_DIR ${APP_DIR}.backup.$(date +%s)
+fi
+mkdir -p $APP_DIR
+tar -xzf app-$COMMIT_ID.tar.gz -C $APP_DIR
+cd $APP_DIR
+sudo systemctl restart fastapi
+echo "Deployment complete!"
